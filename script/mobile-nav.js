740f32c5db24ad904fdd675ce2c0a727b3ba626a
Liam Hayden <l.hayden67@outlook.com>
2022-03-17 14:11:28 +0000

    Added html validation and email validation

--- script/mobile-nav.js
+++ script/mobile-nav.js
@@ -1,4 +1,4 @@
-// animation
+// change navbar
 function navBarAnimation(x) {
   // variables 
   let dropNavSquares = document.getElementById("mobileMenu");
@@ -12,4 +12,17 @@ function navBarAnimation(x) {
   } else {
     dropNavSquares.style.display = "flex";
   }
+}
+
+// form validaiton - form pag and footer
+function validateForm() {
+  // variables
+  let email = document.forms["form"]["email"].value;
+
+  // functionality
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return true;
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
 }
